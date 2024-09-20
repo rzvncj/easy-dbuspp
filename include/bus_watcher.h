10a76e075a53d8697af7ffb4ac2c5393116e3edc
Razvan Cojocaru <rzvncj@gmail.com>
2024-09-20 11:16:47 +0300

    Add support for notifications on bus name disappearance

--- include/bus_watcher.h
+++ include/bus_watcher.h
@@ -20,6 +20,7 @@
 #include "types.h"
 #include <chrono>
 #include <condition_variable>
+#include <functional>
 #include <gio/gio.h>
 #include <string>
 
@@ -36,8 +37,10 @@ public:
      *
      * @param bus_type The type of bus to listen on (session or system).
      * @param bus_name The name we want to make sure is available.
+     * @param start    If this is true, try to automatically start the service that owns the
+     *                 bus name, if activatable.
      */
-    explicit bus_watcher(bus_type_t bus_type, const std::string& bus_name);
+    explicit bus_watcher(bus_type_t bus_type, const std::string& bus_name, bool start = false);
 
     //! Destructor.
     ~bus_watcher();
@@ -50,15 +53,21 @@ public:
     template <typename Rep, typename Period>
     void wait_for(const std::chrono::duration<Rep, Period>& timeout);
 
+    //! (Optionally) set a function to be called when the bus name disappears.
+    void bus_name_disappeared_handler(const std::function<void()>& disappeared_handler);
+
 private:
     static void on_name_appeared(GDBusConnection* connection, const gchar* name, const gchar* name_owner,
                                  gpointer user_data);
+
+    static void on_name_disappeared(GDBusConnection* connection, const gchar* name, gpointer user_data);
 
 private:
     std::condition_variable name_appeared_cv_;
     std::mutex              name_appeared_cv_mutex_;
     bool                    name_appeared_ {false};
     guint                   watcher_id_ {0};
+    std::function<void()>   disappeared_handler_;
 };
 
 } // end of namespace easydbuspp
--- src/bus_watcher.cpp
+++ src/bus_watcher.cpp
@@ -19,15 +19,22 @@
 
 namespace easydbuspp {
 
-bus_watcher::bus_watcher(bus_type_t bus_type, const std::string& bus_name)
+bus_watcher::bus_watcher(bus_type_t bus_type, const std::string& bus_name, bool start)
 {
-    watcher_id_ = g_bus_watch_name(to_g_bus_type(bus_type), bus_name.c_str(), G_BUS_NAME_WATCHER_FLAGS_AUTO_START,
-                                   on_name_appeared, nullptr, this, nullptr);
+    watcher_id_ = g_bus_watch_name(to_g_bus_type(bus_type), bus_name.c_str(),
+                                   start ? G_BUS_NAME_WATCHER_FLAGS_AUTO_START : G_BUS_NAME_WATCHER_FLAGS_NONE,
+                                   on_name_appeared, on_name_disappeared, this, nullptr);
 }
 
 bus_watcher::~bus_watcher()
 {
     g_bus_unwatch_name(watcher_id_);
+}
+
+void bus_watcher::bus_name_disappeared_handler(const std::function<void()>& disappeared_handler)
+{
+    std::lock_guard lock {name_appeared_cv_mutex_};
+    disappeared_handler_ = disappeared_handler;
 }
 
 void bus_watcher::on_name_appeared(GDBusConnection* /* connection */, const gchar* /* name */,
@@ -38,6 +45,18 @@ void bus_watcher::on_name_appeared(GDBusConnection* /* connection */, const gcha
     std::lock_guard lock {watcher->name_appeared_cv_mutex_};
     watcher->name_appeared_ = true;
     watcher->name_appeared_cv_.notify_all();
+}
+
+void bus_watcher::on_name_disappeared(GDBusConnection* /* connection */, const gchar* /* name */, gpointer user_data)
+{
+    bus_watcher* watcher = static_cast<bus_watcher*>(user_data);
+
+    std::lock_guard lock {watcher->name_appeared_cv_mutex_};
+
+    if (watcher->name_appeared_ && watcher->disappeared_handler_)
+        watcher->disappeared_handler_();
+
+    watcher->name_appeared_ = false;
 }
 
 } // end of namespace easydbuspp
